2dc404ef3a665ecabb7ced469badad004a22d2fa
Alex <agkim@lbl.gov>
2025-09-14 19:33:00 +0200

    get rid of unneeded path

--- g2/sources/agn.py
+++ g2/sources/agn.py
@@ -16,7 +16,7 @@ import sys
 import os
 
 # Add parent directory to path to import source module
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from .source import ChaoticSource
 
 
